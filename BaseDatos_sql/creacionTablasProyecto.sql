0faaedf14425183f689f6e1d1b1b13b5a8283a55
proyectoudaleku02 <1glm02@a224m02.local>
2015-05-08 12:17:58 +0200

    INICIO

---
+++ BaseDatos_sql/creacionTablasProyecto.sql
@@ -0,0 +1,151 @@
+DROP TABLE SORTEOS CASCADE CONSTRAINTS;
+
+CREATE TABLE SORTEOS (
+  idSorteo NUMBER,
+  numSorteo NUMBER,
+  cadenciaSorteo NUMBER,
+  fechaApertura DATE,
+  fechaSorteo DATE,
+  fechaCierre DATE,
+  CONSTRAINT sorteos_pk PRIMARY KEY (idSorteo));
+
+DROP TABLE SOLICITUDES CASCADE CONSTRAINTS; 
+ 
+CREATE TABLE SOLICITUDES (
+  idSolicitud NUMBER,
+  fechaHoraCita DATE NULL,
+  situacionSolic VARCHAR2(45) NOT NULL,
+  ordenSorteo NUMBER NULL,
+  CONSTRAINT solicitudes_pk PRIMARY KEY (idSolicitud));
+
+DROP TABLE PROVINCIAS CASCADE CONSTRAINTS; 
+  
+CREATE TABLE PROVINCIAS (
+  idProvincia VARCHAR2(3),
+  nombreProv VARCHAR2(45) NOT NULL,
+  CONSTRAINT provincias_pk PRIMARY KEY (idProvincia));
+
+DROP TABLE CENTROS CASCADE CONSTRAINTS; 
+  
+CREATE TABLE CENTROS (
+  idCentro NUMBER,
+  nombreCent VARCHAR2(45) NOT NULL,
+  modeloCent VARCHAR2(3) NOT NULL,
+  idProvincia VARCHAR2(3),
+  CONSTRAINT centros_pk PRIMARY KEY (idCentro),
+  CONSTRAINT centProv_fk FOREIGN KEY (idProvincia)
+    REFERENCES PROVINCIAS (idProvincia));
+
+DROP TABLE INSCRIPCIONES CASCADE CONSTRAINTS; 
+
+CREATE TABLE INSCRIPCIONES (
+  idInscripcion VARCHAR2(10),
+  idSolicitud NUMBER,
+  CONSTRAINT inscripciones_pk PRIMARY KEY (idInscripcion),
+  CONSTRAINT insSol_fk FOREIGN KEY (idSolicitud)
+    REFERENCES SOLICITUDES (idSolicitud));
+
+DROP TABLE TUTORES CASCADE CONSTRAINTS; 
+
+CREATE TABLE TUTORES (
+  idSolicitante NUMBER,
+  dniTutor VARCHAR2(13) UNIQUE NOT NULL,
+  nombreTutor VARCHAR2(20) NOT NULL,
+  ape1Tutor VARCHAR2(30) NOT NULL,
+  ape2Tutor VARCHAR2(30) NOT NULL,
+  idInscrip VARCHAR2(10),
+  CONSTRAINT tutores_pk PRIMARY KEY (idSolicitante),
+  CONSTRAINT tutIns_fk FOREIGN KEY (idInscrip)
+    REFERENCES INSCRIPCIONES (idInscripcion));
+
+DROP TABLE TELEFONOS CASCADE CONSTRAINTS; 
+
+CREATE TABLE TELEFONOS (
+  numeroTelf VARCHAR2(9),
+  idSolicitante NUMBER,
+  CONSTRAINT telefonos_pk PRIMARY KEY (numeroTelf),
+  CONSTRAINT telTut_fk FOREIGN KEY (idSolicitante)
+    REFERENCES TUTORES (idSolicitante));
+
+DROP TABLE MUNICIPIOS CASCADE CONSTRAINTS; 
+
+CREATE TABLE MUNICIPIOS (
+  idMunicipio NUMBER,
+  nombreMunic VARCHAR2(45) NOT NULL,
+  idProvincia VARCHAR2(3),
+  CONSTRAINT municipios_pk PRIMARY KEY (idMunicipio),
+  CONSTRAINT munProv_fk FOREIGN KEY (idProvincia)
+    REFERENCES PROVINCIAS (idProvincia));
+
+DROP TABLE VIAS CASCADE CONSTRAINTS; 
+
+CREATE TABLE VIAS (
+  idVia NUMBER,
+  tipoVia VARCHAR2(30) NOT NULL,
+  nombreVia VARCHAR2(45) NOT NULL,
+  idMunicipio NUMBER,
+  CONSTRAINT vias_pk PRIMARY KEY (idVia),
+  CONSTRAINT viasMun_fk FOREIGN KEY (idMunicipio)
+    REFERENCES MUNICIPIOS (idMunicipio));
+
+DROP TABLE DIRECCIONES CASCADE CONSTRAINTS; 
+
+CREATE TABLE DIRECCIONES (
+  idDireccion VARCHAR2(40),
+  numDir VARCHAR2(3),
+  letra VARCHAR2(1),
+  piso VARCHAR2(2),
+  escalera VARCHAR2(10),
+  mano VARCHAR2(3),
+  cp VARCHAR2(5) NOT NULL,
+  idVia NUMBER,
+  CONSTRAINT direcciones_pk PRIMARY KEY (idDireccion),
+  CONSTRAINT dirVias_fk FOREIGN KEY (idVia)
+    REFERENCES VIAS (idVia));
+
+DROP TABLE MENORES CASCADE CONSTRAINTS; 
+
+CREATE TABLE MENORES (
+  idSolicitante NUMBER,
+  dniMenor VARCHAR2(13) UNIQUE,
+  nombreMenor VARCHAR2(20) NOT NULL,
+  apel1Menor VARCHAR2(30) NOT NULL,
+  apel2Menor VARCHAR2(30) NOT NULL,
+  sexoMenor VARCHAR2(1) NOT NULL,
+  fechaNacMenor DATE NOT NULL,
+  discapacidadMenor VARCHAR2(2) NOT NULL,
+  idInscrip VARCHAR2(10),
+  idDir VARCHAR2(40),
+  CONSTRAINT menores_pk PRIMARY KEY (idSolicitante),
+  CONSTRAINT menIns_fk FOREIGN KEY (idInscrip)
+    REFERENCES INSCRIPCIONES (idInscripcion),
+  CONSTRAINT menDir_fk FOREIGN KEY (idDir)
+    REFERENCES DIRECCIONES (idDireccion),
+  CONSTRAINT discapacidadMenor_ck CHECK(lower(discapacidadMenor) IN('si','no')));
+
+
+CREATE SEQUENCE idSolicitud_seq INCREMENT BY 1 START WITH 1;
+		
+CREATE SEQUENCE idCentro_seq INCREMENT BY 1 START WITH 1;
+
+CREATE SEQUENCE idSolicitante_seq INCREMENT BY 1 START WITH 1;
+
+CREATE SEQUENCE idMunicipio_seq INCREMENT BY 1 START WITH 1;
+
+CREATE SEQUENCE idSolicitante_seq INCREMENT BY 1 START WITH 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javaUdaleku/proyectoUdaleku/src/proyectoudaleku/Main.java
@@ -0,0 +1,35 @@
+
+package proyectoudaleku;
+
+import gui.*;
+
+
+public class Main {
+
+    private static Inicio inic;
+    private static panInicio panInic;
+    private static panInscripcion panInscrip;
+    
+    public static void main(String[] args) {
+        panInic = new panInicio();
+        inic=new Inicio(); inic.setContentPane(panInic); panInic.setVisible(true);inic.setVisible(true);
+
+    }
+
+    public static void verPanInscrip() {
+        panInscrip = new panInscripcion();
+        inic.getContentPane().setVisible(false);
+        inic.setContentPane(panInscrip);
+        panInscrip.setVisible(true);
+    }
+
+    public static void cancelarPanel() {
+        inic.getContentPane().setVisible(false);
+        inic.setContentPane(panInic);panInic.setVisible(true);
+    }
+
+    public static void salir() {
+        System.exit(0);
+    }
+    
+}
